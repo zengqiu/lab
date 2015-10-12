0ed1f4a349c1c55839b9ca56cf939cb84e16a075
zengqiu <zengqiu@qq.com>
2015-10-12 20:45:47 +0800

    Modified some comments

--- snake.c
+++ snake.c
@@ -1,3 +1,5 @@
+//author: zengqiu
+
 #include <stdio.h>
 #include <windows.h>
 #include <conio.h>
@@ -47,49 +49,49 @@ void goto_pos(HANDLE hOut, int x, int y)
     SetConsoleCursorPosition(hOut, pos);
 }
 
-/*初始化游戏窗口**/
+/*初始化游戏窗口*/
 void init_frame()
 {
-	int i = 0;
-	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);    //定义显示器句柄变量
+    int i = 0;
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);    //定义显示器句柄变量
 
-	set_color_red();
+    set_color_red();
 
-	goto_pos(hOut, INIT_X + 2 * WIDTH + 6, INIT_Y + 2);    //打印游戏名称
-	printf("贪吃蛇游戏");
+    goto_pos(hOut, INIT_X + 2 * WIDTH + 6, INIT_Y + 2);    //打印游戏名称
+    printf("贪吃蛇游戏");
 
-	set_color_green();
+    set_color_green();
 
-	goto_pos(hOut, INIT_X + 2 * WIDTH + 3, INIT_Y + 4);    //打印选择菜单
-	printf("方向：↑↓←→");
+    goto_pos(hOut, INIT_X + 2 * WIDTH + 3, INIT_Y + 4);    //打印选择菜单
+    printf("方向：↑↓←→");
 
-	goto_pos(hOut, INIT_X + 2 * WIDTH + 3, INIT_Y + 6);
-	printf("退出：Esc");
+    goto_pos(hOut, INIT_X + 2 * WIDTH + 3, INIT_Y + 6);
+    printf("退出：Esc");
 
-	//打印上横框
-	for(i = 0; i < 2 * WIDTH; i += 2)
-	{
-		goto_pos(hOut, INIT_X + i, INIT_Y);
-		printf("*");
-	}
-	//打印下横框
-	for(i = 0; i < 2 * WIDTH; i += 2)
-	{
-		goto_pos(hOut, INIT_X + i, INIT_Y + HEIGHT -1);
-		printf("*");
-	}
-	//打印左竖框
-	for(i = 1; i < 20; i++)
-	{
-		goto_pos(hOut, INIT_X, INIT_Y + i);
-		printf("*");
-	}
-	//打印右竖框
-	for(i = 1; i < 20; i++)
-	{
-		goto_pos(hOut, INIT_X + 2 * WIDTH - 2, INIT_Y + i);
-		printf("*");
-	}
+    //打印上横框
+    for(i = 0; i < 2 * WIDTH; i += 2)
+    {
+        goto_pos(hOut, INIT_X + i, INIT_Y);
+        printf("*");
+    }
+    //打印下横框
+    for(i = 0; i < 2 * WIDTH; i += 2)
+    {
+        goto_pos(hOut, INIT_X + i, INIT_Y + HEIGHT - 1);
+        printf("*");
+    }
+    //打印左竖框
+    for(i = 1; i < 20; i++)
+    {
+        goto_pos(hOut, INIT_X, INIT_Y + i);
+        printf("*");
+    }
+    //打印右竖框
+    for(i = 1; i < 20; i++)
+    {
+        goto_pos(hOut, INIT_X + 2 * WIDTH - 2, INIT_Y + i);
+        printf("*");
+    }
 }
 
 /*初始化蛇*/
@@ -104,7 +106,7 @@ pSNAKE init_snake()
         exit(-1);
     }
 
-	//蛇不能在边框上
+    //蛇不能在边框上
     pHead -> x = (rand() % (WIDTH - 2)) * 2 + 2;
     pHead -> y = rand() % (HEIGHT - 2) + 1;
     pHead -> next = NULL;
@@ -120,23 +122,23 @@ FOOD init_food(HANDLE hOut, pSNAKE pHead)
     pSNAKE pSnake = pHead;
     srand((unsigned)time(NULL) + 1);
 
-	while (1)
-	{
-		food.x = (rand() % (WIDTH - 2)) * 2 + 2;
-    	food.y = rand() % (HEIGHT - 2) + 1;
+    while (1)
+    {
+        food.x = (rand() % (WIDTH - 2)) * 2 + 2;
+        food.y = rand() % (HEIGHT - 2) + 1;
 
-		//如果食物在蛇身上
-	    while (pSnake != NULL)
-	    {
-	    	if (food.x == pSnake -> x && food.y == pSnake -> y) break;
-			pSnake = pSnake -> next;
-		}
-		
-		if (pSnake == NULL) break;
-	}
+        //如果食物在蛇身上
+        while (pSnake != NULL)
+        {
+            if (food.x == pSnake -> x && food.y == pSnake -> y) break;
+            pSnake = pSnake -> next;
+        }
+        
+        if (pSnake == NULL) break;
+    }
     
-	goto_pos(hOut, food.x, food.y);
-	printf("■");
+    goto_pos(hOut, food.x, food.y);
+    printf("■");
 
     return food;
 }
@@ -144,128 +146,128 @@ FOOD init_food(HANDLE hOut, pSNAKE pHead)
 /*吃食物*/
 void eat_food(HANDLE hOut, pSNAKE pHead, pFOOD pFood)
 {
-	//如果蛇头的位置与食物的位置相同
-	if (pHead -> x == pFood -> x && pHead -> y == pFood -> y)
-	{
-		pSNAKE pEnd = (pSNAKE)malloc(sizeof(SNAKE));
-		pSNAKE pSnake = pHead;
+    //如果蛇头的位置与食物的位置相同
+    if (pHead -> x == pFood -> x && pHead -> y == pFood -> y)
+    {
+        pSNAKE pEnd = (pSNAKE)malloc(sizeof(SNAKE));
+        pSNAKE pSnake = pHead;
 
-		if (pEnd == NULL)
-		{
-			printf("内存分配失败！\n");
+        if (pEnd == NULL)
+        {
+            printf("内存分配失败！\n");
             exit(-1);
-		}
+        }
 
-		//指向蛇尾
-		while (pSnake -> next != NULL) pSnake = pSnake -> next;
+        //指向蛇尾
+        while (pSnake -> next != NULL) pSnake = pSnake -> next;
 
-		//在蛇尾插入新节点
-		pEnd -> pre = pSnake;
-		pEnd -> next = NULL;
-		pEnd -> x = pSnake -> x;
-		pEnd -> y = pSnake -> y;
-		pSnake -> next = pEnd;
-	}
+        //在蛇尾插入新节点
+        pEnd -> pre = pSnake;
+        pEnd -> next = NULL;
+        pEnd -> x = pSnake -> x;
+        pEnd -> y = pSnake -> y;
+        pSnake -> next = pEnd;
+    }
 }
 
-/*按键*/
+/*按键获取*/
 char press_keyboard(char c)
 {
-	char key;
-	if (kbhit())
-	{
-		key = getch();
-	
-		if (c != 80 && key== 72) c = key;          //向上移动
-		else if (c != 72 && key == 80) c = key;    //向下移动
-		else if (c != 77 && key == 75) c = key;    //向左移动
-		else if (c != 75 && key == 77) c = key;    //向右移动
-		else if (key == 27) c = key;               //按 ESC 键退出
-	}
+    char key;
+    if (kbhit())
+    {
+        key = getch();
+    
+        if (c != 80 && key== 72) c = key;          //向上移动
+        else if (c != 72 && key == 80) c = key;    //向下移动
+        else if (c != 77 && key == 75) c = key;    //向左移动
+        else if (c != 75 && key == 77) c = key;    //向右移动
+        else if (key == 27) c = key;               //按 ESC 键退出
+    }
 
-	return c;
+    return c;
 }
 
 /*移动蛇*/
 int move_snake(HANDLE hOut, pSNAKE pHead, pFOOD pFood, char c)
 {
-	int game_over = 0;
+    int game_over = 0;
 
-	pSNAKE pSnake = pHead;
-	int end_x = 0, end_y = 0, pre_x = 1, pre_y = 1;
+    pSNAKE pSnake = pHead;
+    int end_x = 0, end_y = 0, pre_x = 1, pre_y = 1;
 
-	//指向蛇尾
-	while (pSnake -> next != NULL) pSnake = pSnake -> next;
-	
-	//如果最后一个节点不是头节点
-	if (pSnake != pHead)
-	{
-		end_x = pSnake -> x;
-		end_y = pSnake -> y;
-		pre_x = pSnake -> pre -> x;
-		pre_y = pSnake -> pre -> y;
-	}
-	
-	//清除蛇尾
-	goto_pos(hOut, pSnake -> x, pSnake -> y);
-	printf("  ");
+    //指向蛇尾
+    while (pSnake -> next != NULL) pSnake = pSnake -> next;
+    
+    //如果最后一个节点不是头节点
+    if (pSnake != pHead)
+    {
+        end_x = pSnake -> x;
+        end_y = pSnake -> y;
+        pre_x = pSnake -> pre -> x;
+        pre_y = pSnake -> pre -> y;
+    }
+    
+    //清除蛇尾
+    goto_pos(hOut, pSnake -> x, pSnake -> y);
+    printf("  ");
 
-	//从蛇尾向蛇头前进
-	while (pSnake != pHead)
-	{
-		pSnake -> x = pSnake -> pre -> x;
-		pSnake -> y = pSnake -> pre -> y;
-		pSnake = pSnake -> pre;
-	}
+    //从蛇尾向蛇头前进
+    while (pSnake != pHead)
+    {
+        pSnake -> x = pSnake -> pre -> x;
+        pSnake -> y = pSnake -> pre -> y;
+        pSnake = pSnake -> pre;
+    }
 
-	switch(c)
-	{
-		case 72:    //向上移动
-			pHead -> y -= 1;
-			if (pHead -> y <= 0) pHead -> y += (HEIGHT - 2);
-			break;
-		case 80:    //向下移动
-			pHead -> y += 1;
-			if (pHead -> y >= HEIGHT - 1) pHead -> y -= (HEIGHT - 2);
-			break;
-		case 75:    //向左移动
-			pHead -> x -= 2;
-			if (pHead -> x <= 0) pHead -> x += (WIDTH * 2 - 4);
-			break;
-		case 77:    //向右移动
-			pHead -> x += 2;
-			if (pHead -> x >= WIDTH * 2 - 2) pHead -> x -= (WIDTH * 2 -4);
-			break;
-	}
-	
-	//如果最后一个节点的坐标与其前一个节点坐标相同，则说明刚吃完食物，初始化一个食物
-	if (end_x == pre_x && end_y == pre_y)
-	{
-		*pFood = init_food(hOut, pHead);
-	}
+    switch(c)
+    {
+        case 72:    //向上移动
+            pHead -> y -= 1;
+            if (pHead -> y <= 0) pHead -> y += (HEIGHT - 2);
+            break;
+        case 80:    //向下移动
+            pHead -> y += 1;
+            if (pHead -> y >= HEIGHT - 1) pHead -> y -= (HEIGHT - 2);
+            break;
+        case 75:    //向左移动
+            pHead -> x -= 2;
+            if (pHead -> x <= 0) pHead -> x += (WIDTH * 2 - 4);
+            break;
+        case 77:    //向右移动
+            pHead -> x += 2;
+            if (pHead -> x >= WIDTH * 2 - 2) pHead -> x -= (WIDTH * 2 -4);
+            break;
+    }
+    
+    //如果最后一个节点的坐标与其前一个节点坐标相同，则说明刚吃完食物，初始化一个食物
+    if (end_x == pre_x && end_y == pre_y)
+    {
+        *pFood = init_food(hOut, pHead);
+    }
 
-	//当蛇头碰到蛇身时游戏结束
-	pSnake = pHead -> next;
-	while (pSnake != NULL)
-	{
-		if (pSnake -> x == pHead  -> x && pSnake -> y == pHead -> y) game_over = 1;
+    //当蛇头碰到蛇身时游戏结束
+    pSnake = pHead -> next;
+    while (pSnake != NULL)
+    {
+        if (pSnake -> x == pHead  -> x && pSnake -> y == pHead -> y) game_over = 1;
         pSnake = pSnake -> next;
-	}
+    }
 
-	return game_over;
+    return game_over;
 }
 
 /*打印蛇*/
 void print_snake(HANDLE hOut, pSNAKE pHead)
 {
-	pSNAKE pSnake = pHead;
-	while (pSnake != NULL)
-	{
-		goto_pos(hOut, pSnake -> x, pSnake -> y);
-		if (pSnake == pHead) printf("■");
-		else printf("□");
-		pSnake = pSnake -> next;
-	}
+    pSNAKE pSnake = pHead;
+    while (pSnake != NULL)
+    {
+        goto_pos(hOut, pSnake -> x, pSnake -> y);
+        if (pSnake == pHead) printf("■");
+        else printf("□");
+        pSnake = pSnake -> next;
+    }
 }
 
 /*释放内存*/
@@ -289,37 +291,36 @@ void free_memory(pSNAKE *pHead)
 
 int main()
 {
-	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE); //定义显示器句柄变量
-	int game_over = 0;
-	char c = 77, key;    //初始化运行方向向右
-
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE); //定义显示器句柄变量
+    int game_over = 0;
+    char c = 77, key;                      //初始化运行方向向右
     init_frame();                          //初始化游戏窗口
     pSNAKE pHead = init_snake();           //初始化蛇
     FOOD food = init_food(hOut, pHead);    //随机产生一个食物
 
     while (1)
     {
-    	eat_food(hOut, pHead, &food);
-    	
-    	key = press_keyboard(c);
-    	if (key == 27)
-    	{
-    		game_over = 1;
-    		break;
-		}
-		else c = key;
+        eat_food(hOut, pHead, &food);
+        
+        key = press_keyboard(c);
+        if (key == 27)
+        {
+            game_over = 1;
+            break;
+        }
+        else c = key;
 
-		game_over = move_snake(hOut, pHead, &food, key);
-		if (game_over) break;
+        game_over = move_snake(hOut, pHead, &food, key);
+        if (game_over) break;
 
-		print_snake(hOut, pHead);
-		
-		Sleep(200);
-	}
+        print_snake(hOut, pHead);
+        
+        Sleep(200);
+    }
 
-	if (game_over)
+    if (game_over)
     {
-    	system("cls");
+        system("cls");
         printf("游戏结束！\n");
 
         free_memory(&pHead);
